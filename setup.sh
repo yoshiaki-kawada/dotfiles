68aac525fc162b9bf1321618d465f5106be40b7f
Yoshiaki Kawada <kawada@emitech.jp>
2019-09-18 22:49:34 +0900

    vscodeの設定を追加

--- setup.sh
+++ setup.sh
@@ -14,5 +14,19 @@ function deploy_iterm2() {
   ln -siv ~/dotfiles/iterm2/com.googlecode.iterm2.plist ~/Library/Preferences
 }
 
+function deploy_vscode() {
+  ln -siv ~/dotfiles/vscode/settings.json ~/Library/Application\ Support/Code/User
+  ln -siv ~/dotfiles/vscode/keybindings.json ~/Library/Application\ Support/Code/User
+}
+
+function install_vscode_extentions() {
+  cat ./vscode/extensions | while read line
+  do
+    code --install-extension $line
+  done
+}
+
 deploy_dotfiles
 deploy_iterm2
+deploy_vscode
+install_vscode_extentions
