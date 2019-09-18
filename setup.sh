c45196529e8278217336ab2aaad934b5aba48a8b
Yoshiaki Kawada <kawada@emitech.jp>
2019-09-18 18:15:09 +0900

    iterm2の設定方法を変更

--- setup.sh
+++ setup.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-for file in .??*
-do
-  [[ "$file" == ".DS_Store" ]] && continue
-  [[ "$file" == ".git" ]] && continue
+function deploy_dotfiles() {
+  for file in .??*
+    do
+    [[ "$file" == ".DS_Store" ]] && continue
+    [[ "$file" == ".git" ]] && continue
 
-  ln -s ~/dotfiles/$file ~/$file
-done
+    ln -siv ~/dotfiles/$file ~/$file
+  done
+}
 
+function deploy_iterm2() {
+  ln -siv ~/dotfiles/iterm2/com.googlecode.iterm2.plist ~/Library/Preferences
+}
+
+deploy_dotfiles
+deploy_iterm2
